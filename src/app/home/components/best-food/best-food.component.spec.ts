d9ed50c9edad1ae20a818d8bdb7779c3807a26d0
Joydip Paul <joydippaul72@gmail.com>
2023-01-21 02:13:32 +0600

    hero

---
+++ src/app/home/components/best-food/best-food.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BestFoodComponent } from './best-food.component';
+
+describe('BestFoodComponent', () => {
+  let component: BestFoodComponent;
+  let fixture: ComponentFixture<BestFoodComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BestFoodComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BestFoodComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
